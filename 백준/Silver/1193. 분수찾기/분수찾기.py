0a05abf061622e53809aea7584a1dc168c2b8621
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2025-09-24 17:21:19 +0900

    [Silver V] Title: 분수찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,17 +1,16 @@
 X = int(input())
 Y = X
 count_X = 0
-if (X == 1):
+if (X == 1): # 1인 경우 
     print(f"{1}/{1}")
-for i in range(1,X,1) :
+for i in range(1,X,1) : # 1이 아닌 경우
     if(Y-i > i+1):
         Y = Y - i
         count_X += 1
         continue
     else :
-        count_X = count_X
-        if(count_X %2 == 1): #홀수
+        if(count_X %2 == 1): # 라인이 홀수일때
             print(f"{2-Y + 2*i}/{Y-i}")
-        else :
+        else : # 라인이 짝수일때
             print(f"{Y-i}/{2-Y + 2*i}")
         break
