343e4418a3fdcd48933f0627eb7cfab2de24957b
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2025-12-29 15:30:34 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 32544 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,11 +1,8 @@
 N = int(input())
 a = list(input())
 sum = 0
+mod = 1234567891
 # print(ord(a[0])-96)
 for i in range(len(a)):
-    sum += (ord(a[i])-96) * (31**i)
-if(sum < 1234567891):
-    print(sum)
-else:
-    sum = sum // 1234567891
-    print(sum)
+    sum = (sum + (ord(a[i])-96) * (31**i)) % mod
+print(sum)
