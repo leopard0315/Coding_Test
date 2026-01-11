29a42f3fff89487d290beabd4a0360406b9bcc3f
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-01-11 16:21:31 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,17 @@
+N = int(input())
+for i in range(N):
+    cl = input()
+    if(cl == "Algorithm"):
+        print(204)
+    elif(cl == "DataAnalysis"):
+        print(207)
+    elif(cl == "ArtificialIntelligence"):
+        print(302)
+    elif(cl == "CyberSecurity"):
+        print("B101")
+    elif(cl == "Network"):
+        print(303)
+    elif(cl == "Startup"):
+        print(501)
+    elif(cl == "TestStrategy"):
+        print(105)
