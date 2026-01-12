fb0c6907ec45d70a2d160c05b73f07c1ca3e58d3
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-01-12 11:35:08 +0900

    [Bronze I] Title: UCPC는 무엇의 약자일까?, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/15904. UCPC는 무엇의 약자일까？/UCPC는 무엇의 약자일까？.py
+++ 백준/Bronze/15904. UCPC는 무엇의 약자일까？/UCPC는 무엇의 약자일까？.py
@@ -1,20 +1,16 @@
-a = list(map(str,input().split()))
-b = []
-string = ""
-for i in range(len(a)): # A~Z
-    for j in range(len(a[i])):
-        if(ord(a[i][j:j+1]) >= 65 and ord(a[i][j:j+1])<= 90):
-            string += (a[i][j:j+1])
-
-target = "UCPC"
-idx = 0
-for ch in string:
-    if ch == target[idx]:
-        idx += 1
-        if idx == 4:
-            break
-
-if(idx == 4):
+s = input()
+ucpc = 0
+for ch in s:
+    if ucpc == 0 :
+        if ch == 'U':
+            ucpc += 1
+    elif ucpc == 1 or ucpc == 3:
+        if ch == 'C':
+            ucpc += 1
+    elif ucpc == 2:
+        if ch == 'P':
+            ucpc += 1
+if ucpc == 4:
     print("I love UCPC")
-else:
+else :
     print("I hate UCPC")
