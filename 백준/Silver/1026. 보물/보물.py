388cd6301b390150bbd3166a55624113351450a4
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-01-16 20:44:01 +0900

    Add b_list rearrangement or not method
    Refactor to calculate minimum sum using sorted lists.

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -13,3 +13,21 @@ S = 0
 for i in range(N):
     S = S + A[i] * B[N-i-1]
 print(S) 
+
+# b안의 수를 재배열하지 않을때
+# -> 새로 리스트를 만들어서 정렬해서 적용하기-> sorted() 이용
+# 입력값 받기
+N = int(input())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+
+# A,B 정렬
+A.sort()
+C = sorted(B)
+C.sort(reverse=True)
+
+# S의 최솟값 구하기
+S = 0
+for i in range(N):
+    S = S + A[i] * C[i]
+print(S)
