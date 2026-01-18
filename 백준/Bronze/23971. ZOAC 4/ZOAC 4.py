642ec563ec8f4f6817660740d67dc2495256f8a6
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-01-18 23:17:46 +0900

    [Bronze III] Title: ZOAC 4, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.py
@@ -0,0 +1,16 @@
+# 23971번 백준
+# 입력 & 배열 생성
+H,W,N,M = map(int,input().split())
+cnt1 = 0
+cnt2 = 0
+
+# 가로 카운트
+for i in range(0,W,M+1):
+    cnt1 += 1
+
+# 세로 카운트
+for j in range(0,H,N+1):
+    cnt2 += 1
+
+# 총합 계산
+print(cnt1 * cnt2)
