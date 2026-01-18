940c3c3c65178b7d647b955f0cc43c91ec6827c5
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-01-18 23:18:36 +0900

    [Bronze III] Title: ZOAC 4, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/23971. ZOAC 4/ZOAC 4.py
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.py
@@ -4,13 +4,13 @@ H,W,N,M = map(int,input().split())
 cnt1 = 0
 cnt2 = 0
 
-# 가로 카운트
+# 가로 카운트 값
 for i in range(0,W,M+1):
     cnt1 += 1
 
-# 세로 카운트
+# 세로 카운트 값
 for j in range(0,H,N+1):
     cnt2 += 1
 
 # 총합 계산
-print(cnt1 * cnt2)
+print(cnt1 * cnt2) 
