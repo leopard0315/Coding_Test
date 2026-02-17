187c38c33a81943876f0e02d6ed9bc65874402ab
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-02-17 23:31:52 +0900

    [Bronze I] Title: 증가 수열, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/30236. 증가 수열/증가 수열.py
+++ 백준/Bronze/30236. 증가 수열/증가 수열.py
@@ -11,15 +11,17 @@ for _ in range(t):
     # 배열 생성
     a = list(map(int,input().split()))
     b = [0] * len(a)
-    c = 1
-    
-    if(a[0] == 1):
-        c += 1    
-    b[0] = c
-    for i in range(1,len(a)):
+    c = 0
+
+    # 배열 비교
+    for i in range(0,len(a)):
+        # 해당 배열 값과 +1 증가한 값이 동일할때 +2 증가 
+        # 아니면 그대로 +1 증가
         if(a[i] == (c + 1)):
             c += 2
         else :
             c += 1
         b[i] = c
+        
+    # 최솟값 출력
     print(b[-1])
