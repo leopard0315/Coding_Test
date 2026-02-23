102ac98cae6181bcc74a4a11002f628dc0d3146b
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-02-23 12:24:01 +0900

    [Silver V] Title: 숫자 카드, Time: 536 ms, Memory: 122120 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -9,6 +9,7 @@ n = set(map(int,input().split()))
 M = int(input())
 m = list(map(int,input().split()))
 
+# set은 자체적인 해시함수로 인하여 해당 값의 주소를 정확하게 찾는다.(O(1)이다.)
 # set안에 해당 리스트 값이 있는지 확인
 c = []
 for i in range(M):
