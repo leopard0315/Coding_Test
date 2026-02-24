a161dc8aec7d0cd6bdd14f33534068afabf01afd
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-02-24 12:15:47 +0900

    [Silver IV] Title: 대칭 차집합, Time: 204 ms, Memory: 72456 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -1,13 +1,13 @@
 # 백준 1269번
 # 대칭 차집합
-# import sys
-# input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
 # 입력
-a,b = map(int,input().split())
+a,b = map(int,input().strip().split())
 # A - set, B - list
-A = set(map(int,input().split()))
-B = list(map(int,input().split()))
+A = set(map(int,input().strip().split()))
+B = list(map(int,input().strip().split()))
 C = set()
 
 # 교집합 구하기
@@ -17,9 +17,6 @@ for i in range(b):
         C.add(B[i])
 
 # A,B,교집합 길이 구하기
-# print(len(A))
-# print(len(B))
-# print(len(C))
 minus1 = len(A) - len(C)
 minus2 = len(B) - len(C)
 print(minus1 + minus2)
