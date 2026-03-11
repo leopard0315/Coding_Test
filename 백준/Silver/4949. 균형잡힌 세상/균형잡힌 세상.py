e7ed3c7843a3ef08ee4ebd1f477f532905138a6b
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-03-12 00:55:24 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 84 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,7 +1,9 @@
 # 백준 4949번
 # 균형잡힌 세상
+
 import sys
 input = sys.stdin.readline
+
 # 입력
 while (1):
     N = input()
@@ -13,30 +15,26 @@ while (1):
         for i in range(len(N)):
             if N[i] == "(":
                 a.append(N[i])
+
             elif N[i] == ")":
-                if a :
-                    if (a[-1] == "("):
-                        a.pop()
-                    else:
-                        answer = "No"
-                        break
+                if a and (a[-1] == "(") :
+                    a.pop()
                 else:
                     answer = "No"
                     break
+
             elif N[i] == "[":
                 a.append(N[i])
+
             elif N[i] == "]":
-                if a :
-                    if (a[-1] == "["):
-                        a.pop()
-                    else:
-                        answer = "No"
-                        break
+                if a and (a[-1] == "["):
+                    a.pop()
                 else:
                     answer = "No"
                     break
             else:
                 continue
+        # 출력
         if len(a) == 0 and answer == "":
             print("yes")
         else :
