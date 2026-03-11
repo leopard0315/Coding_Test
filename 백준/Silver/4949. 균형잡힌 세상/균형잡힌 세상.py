6348411d24afcaaad750284e7951d02d190cf1bd
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-03-12 00:53:19 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 80 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,43 @@
+# 백준 4949번
+# 균형잡힌 세상
+import sys
+input = sys.stdin.readline
+# 입력
+while (1):
+    N = input()
+    answer = ""
+    if N.rstrip() == ".":
+        break
+    else:
+        a = [] # 소괄호 & 대괄호 순서 중요
+        for i in range(len(N)):
+            if N[i] == "(":
+                a.append(N[i])
+            elif N[i] == ")":
+                if a :
+                    if (a[-1] == "("):
+                        a.pop()
+                    else:
+                        answer = "No"
+                        break
+                else:
+                    answer = "No"
+                    break
+            elif N[i] == "[":
+                a.append(N[i])
+            elif N[i] == "]":
+                if a :
+                    if (a[-1] == "["):
+                        a.pop()
+                    else:
+                        answer = "No"
+                        break
+                else:
+                    answer = "No"
+                    break
+            else:
+                continue
+        if len(a) == 0 and answer == "":
+            print("yes")
+        else :
+            print("no")
