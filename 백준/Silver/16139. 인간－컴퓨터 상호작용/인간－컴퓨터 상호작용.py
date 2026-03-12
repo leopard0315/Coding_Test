39b86eee5e30cd4bb72e87efee6651e26cbd437d
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-03-12 23:49:05 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 172 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -8,11 +8,9 @@ S = input()
 q = int(input())
 for i in range(q):
     a,l,r = map(str,input().split())
-    l = int(l)
-    r = int(r)
 
     cnt = 0
-    for j in range(l,r+1):
+    for j in range(int(l),int(r)+1):
         if S[j] == a:
             cnt += 1
         else:
