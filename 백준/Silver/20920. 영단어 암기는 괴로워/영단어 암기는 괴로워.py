da6fad640bf40fd80fa81b7dec101567289da105
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-03-21 18:07:13 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 352 ms, Memory: 62680 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -1,28 +1,26 @@
 # 백준 20920번
-# 영단어 암기는 어려워
-# 아이디어 : 한 배열을 만들어서 count값과 길이 알파벳 사전순으로 되도록 순서대로 한다.
-
-from collections import Counter
+# 영단어 암기는 괴로워
 import sys
 input = sys.stdin.readline
+from collections import Counter
 
 # 입력
 N,M = map(int,input().strip().split())
-A = []
+a = []
 for i in range(N):
-    n = input().strip()
-    if len(n) < M :
-        continue
-    else: # 단어의 길이가 M이상인 경우
-        A.append(n)
+    word = input().strip()
+    if len(word) < M:
+        pass
+    else: # 단어의 길이가 M보다 크거나 같으면 추가
+        a.append(word)
 
-# Counter사용해서 횟수 카운트하기
-word_cnt = Counter(A)
-word_set = list(word_cnt.items())
+# 단어 횟수 count하기
+word_count = Counter(a)
+word_cnt = list(word_count.items())
 
-# 정렬기준 3가지 / - : desc, + : asc
-word_set.sort(key = lambda x: (-x[1],-len(x[0]),x[0]))
+# 정렬 3가지 기준 정렬 : -x[1]은 단어나온횟수를 기준으로, len(x[0])은 단어의 길이를 기준으로, x[0]은 단어 사전순으로 
+word_cnt.sort(key= lambda x : (-x[1],-len(x[0]),x[0]))
 
 # 출력
-for word,cnt in word_set:
+for word,cnt in word_cnt:
     print(word)
