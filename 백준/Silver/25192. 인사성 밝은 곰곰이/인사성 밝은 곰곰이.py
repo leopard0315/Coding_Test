8b5fd9e6a8ebe8da363c2835e9e5cde72f6891e1
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-03-21 12:33:57 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 84 ms, Memory: 43680 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.py
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.py
@@ -14,14 +14,16 @@ for i in range(N):
 
     # 채팅방에 누군가 들어왔을때
     if state == "ENTER":
-        a = set()
-    else:
-        # 채팅방에 이미 말한 경우
-        if state in a:
-            continue
-        else: # 채팅방에 처음 말한 경우
-            a.add(state)
-            cnt += 1
+        a.clear()
+    # 채팅방에 이미 말한 경우
+    elif (state in a):
+        continue # 여기서는 pass도 가능
+    else: # 채팅방에 처음 말한 경우
+        a.add(state)
+        cnt += 1
 
 # 출력
 print(cnt)
+
+# pass : 그냥 문법적으로 채워두는것
+# clear은 해당 자료구조를 비울때 사용함
