66a680759321960e20d1a05562e7f55f3e399e18
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-03-21 12:19:52 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 2456 ms, Memory: 43680 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.py
@@ -0,0 +1,25 @@
+# 백준 25192번
+# 인사성 밝은 곰곰이
+# 목표 : 채팅방의 기록을 통해 곰곰티콘이 사용된 횟수 구하기
+
+# 입력
+N = int(input())
+a = set()
+cnt = 0
+
+for i in range(N):
+    state = input()
+
+    # 채팅방에 누군가 들어왔을때
+    if state == "ENTER":
+        a = set()
+    else:
+        # 채팅방에 이미 말한 경우
+        if state in a:
+            continue
+        else: # 채팅방에 처음 말한 경우
+            a.add(state)
+            cnt += 1
+
+# 출력
+print(cnt)
