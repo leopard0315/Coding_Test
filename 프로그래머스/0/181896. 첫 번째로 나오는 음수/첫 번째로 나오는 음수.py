2921cce057d4dd172b064fefe79e4d0452755801
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-06-06 16:00:06 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,9 +1,7 @@
 def solution(num_list):
-    answer = -1
     for i in range(len(num_list)):
         if num_list[i] < 0:
-            answer = i
-            break
+            return i
         else:
             continue
-    return answer
+    return -1
