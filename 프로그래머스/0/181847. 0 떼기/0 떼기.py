a9dc5c590242f3cca9b7e0233aa7bbeb511b74ae
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-06-06 16:17:32 +0900

    [level 0] Title: 0 떼기, Time: 0.00 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.py
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.py
@@ -1,10 +1,5 @@
 def solution(n_str):
     n = 0
     for i in range(len(n_str)):
-        if n_str[i] == '0':
-            continue
-        else:
-            n = i
-            break
-        
-    return n_str[n:]
+        if n_str[i] != '0':
+            return n_str[i:]
