68f431e9d1b8c6074e7a7148a5e349c6cc00050b
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-06-07 19:11:05 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.py
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.py
@@ -1,10 +1,7 @@
 def solution(my_string, target):
     answer = 0
-    length = len(target)
-    for i in range(0,len(my_string)):
-        if(my_string[i:i+len(target)] == target):
-            answer = 1
-            break
-        else:
-            continue
+    if target in my_string:
+        answer = 1
+    else:
+        answer = 0
     return answer
