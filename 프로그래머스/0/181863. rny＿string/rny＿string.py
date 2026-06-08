0bbfc5b5c45b4f2c19823327c9e138dfc51fffa7
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-06-08 12:42:43 +0900

    [level 0] Title: rny_string, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.py
+++ 프로그래머스/0/181863. rny＿string/rny＿string.py
@@ -1,10 +1,2 @@
 def solution(rny_string):
-    rny_string = list(rny_string) # 파이썬에서는 문자열은 불변이기 때문에 list로 변환하여 사용한다.
-    
-    for i in range(len(rny_string)): # 문자열 변경
-        if rny_string[i] == 'm':
-            rny_string[i] ='rn'
-             
-    answer = "".join(rny_string) # 문자열 합치기
-    
-    return answer # 반환값 반환하기
+    return rny_string.replace('m','rn')
