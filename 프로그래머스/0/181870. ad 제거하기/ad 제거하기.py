ef89f613bb12ca4f2dbea967b7dd6c4f641d8521
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-06-15 16:05:56 +0900

    [level 0] Title: ad 제거하기, Time: 0.05 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
@@ -0,0 +1,10 @@
+def solution(strArr):
+    answer = []
+    for i in range(0,len(strArr)):
+        if 'ad' in strArr[i]:
+            pass
+        else:
+            answer.append(strArr[i])
+            
+    return answer
+    
