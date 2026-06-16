207aaf6f3ae869685f6664bb5b1c5324424f40b4
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-06-16 18:56:52 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
@@ -1,7 +1,2 @@
 def solution(n):
-    answer = 0
-    if (n%2 != 0): # 홀수일때
-        n -= 1
-    for i in range(n,0,-2):
-        answer += i
-    return answer
+    return sum(i for i in range(2,n+1,2))
