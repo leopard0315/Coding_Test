9ef673f4e2658b6d9158c433f7d8f62ad88d215a
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-07-10 20:52:36 +0900

    [level 0] Title: 369게임, Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.py
+++ 프로그래머스/0/120891. 369게임/369게임.py
@@ -1,7 +1,4 @@
 def solution(order):
     order = str(order)
     answer = 0
-    for i in order:
-        if i == '3' or i == '6' or i == '9':
-            answer += 1
-    return answer
+    return order.count('3') + order.count('6') + order.count('9')
