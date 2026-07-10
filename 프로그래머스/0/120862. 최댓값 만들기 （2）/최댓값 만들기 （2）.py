b865a2592cb4de031d82f216b01d9d8ce1085a48
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-07-10 20:48:11 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    # - 를 유의하기
+    numbers.sort() # 오름차순 정렬
+    a1 = numbers[-1] * numbers[-2]
+    a2 = numbers[0] * numbers[1]
+    if a1 > a2:
+        return a1
+    else:
+        return a2
