e8b2d9a5e11d70a855d956d929b444a0c6902450
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-07-21 17:01:38 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -1,5 +1,3 @@
 def solution(score):
     score2 = sorted([sum(i) for i in score],reverse = True)
-    rank = [score2.index(sum(i))+1 for i in score]
-    
-    return rank
+    return [score2.index(sum(i))+1 for i in score]
