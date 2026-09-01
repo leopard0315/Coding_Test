967096227d9390070fed2c69a932555272f09403
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-09-02 00:59:49 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,7 +1,7 @@
 def solution(my_string):
     answer = ''
     for i in range(len(my_string)):
-        if my_string[i] == 'a' or my_string[i] == 'e' or my_string[i] == 'i' or my_string[i] =='o' or my_string[i] == 'u':
+        if my_string[i] in 'aeiou':
             continue
         else:
             answer += my_string[i]
