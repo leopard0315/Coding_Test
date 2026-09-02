797e5b2d0c8d5b8362a04955a518ef888e684691
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-09-02 12:18:28 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -1,6 +1,4 @@
 def solution(array, height):
-    array.sort()
-    for i in range(len(array)):
-        if array[i] > height:
-            return len(array) - i
-    return 0
+    array.append(height) # 머쓱이 키 추가
+    array.sort(reverse = True) # 머쓱이 키를 추가한후에 거꾸로 정렬하고
+    return array.index(height) # 해당 인덱스를 표현해서 뒤에서 몇번째인지 위치 구하기
