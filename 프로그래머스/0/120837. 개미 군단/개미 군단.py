3b34cfeb9d1da1e59b9bf0fd86606858460e0666
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-09-08 16:03:52 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.py
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -1,14 +1,2 @@
 def solution(hp):
-    answer = 0
-    # 장군개미 5 공격력
-    answer += hp // 5
-    hp %= 5
-    
-    # 병정개미 3 공격력
-    answer += hp // 3
-    hp %= 3
-    
-    # 일개미 1 공격력
-    answer += hp // 1
-    
-    return answer
+    return (hp // 5) + (hp % 5 // 3) + (hp % 5 % 3 // 1)
