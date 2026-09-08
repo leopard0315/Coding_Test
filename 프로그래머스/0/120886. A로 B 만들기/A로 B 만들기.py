2e99205f9f297c0792e6e6a45669ea333fa9a573
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-09-08 17:10:09 +0900

    [level 0] Title: A로 B 만들기, Time: 0.24 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,11 +1,10 @@
 def solution(before, after):
-    b_list = list(before)
-    a_list = list(after)
-    b_list.sort()
-    a_list.sort()
-    b_str = ','.join(b_list)
-    a_str = ','.join(a_list)
-    if b_str == a_str:
+    # 문자열을 리스트로 쪼갠후에 오름차순 정렬
+    b_list = sorted(before)
+    a_list = sorted(after)
+
+    # 재조합된 문자열이 일치하는지 확인
+    if b_list == a_list:
         return 1
     else:
         return 0
