f885c4cdc7d4bf9e48dfa5dc8108edff41da89c7
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-09-12 21:12:43 +0900

    [level 2] Title: H-Index, Time: 0.11 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,4 +1,7 @@
 # 목표 : 과학자의 생산성과 영향력을 나타내는 지표인 H-index : h구하기
+# 논문 n편 중에서 h번이상 인용된 논문이 h편이상이다.
+# -> citations[i] >= (i+1)을 만족하는가? => 만족못할시 그 직전의 값 출력
+# 예외) 만약 동일한 값들로만 이루어져있는경우 -> 배열의 길이 출력
 def solution(citations):
     c = citations
     c.sort(reverse = True)
