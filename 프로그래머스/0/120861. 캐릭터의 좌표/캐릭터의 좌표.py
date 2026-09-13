6f9011d6bbc7b74970841b1af19be5f30fc5af11
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2026-09-13 20:39:10 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.01 ms, Memory: 11.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -0,0 +1,19 @@
+def solution(keyinput, board):
+    answer = [0,0]
+    max_x = board[0] // 2
+    max_y = board[1] // 2
+    
+    for i in keyinput:
+        if i == 'left':
+            if answer[0] > -max_x:
+                answer[0] += -1
+        elif i == 'right':
+            if answer[0] < max_x:
+                answer[0] += 1
+        elif i == 'up':
+            if answer[1] < max_y:
+                answer[1] += 1
+        elif i == 'down':
+            if answer[1] > -max_y:
+                answer[1] += -1
+    return answer
